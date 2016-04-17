8142a1b30cd63e8a1e0251dc09e3a538593c29bd
ramadhanrezza <mrezza.ramadhan@gmail.com>
2016-04-17 18:36:41 +0700

    change display

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -28,39 +28,6 @@
                 </form>
             </div>
         </div>
-        <!-- Static navbar -->
-        <?php /*<nav class="navbar navbar-default panel panel-primary">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="<?php echo base_url() ?>">Scramble Word</a>
-                </div>
-            </div><!--/.container-fluid -->
-        </nav>
-
-        <!-- Main component for a primary marketing message or call to action -->
-        <div class="container">
-            <h4>Silahkan pilih kategori untuk memulai</h4><br>
-            <form class="form-horizontal" method="post" action="">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label text-left">Select Category</label>
-                    <div class="col-sm-4">
-                        <select class="form-control" name="category">
-                            <option value="0">All</option>
-                            <?php foreach ($list_category as $row) : ?>
-                            <option value="<?php echo $row['id'] ?>"><?php echo ucfirst($row['name']) ?></option>
-
-                            <?php endforeach; ?>
-
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-4">
-                        <button class="btn btn-default">Start &raquo;</button>
-                    </div>
-                </div>
-            </form>
-        </div> */ ?>
     </div>
 </body>
 </html>
--- application/views/home/start.php
+++ application/views/home/start.php
@@ -25,35 +25,6 @@
                 </form>    
             </div>
         </div>
-        <!-- Static navbar -->
-        <?php /* <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="<?php echo base_url() ?>">Scramble Word</a>
-                </div>
-            </div><!--/.container-fluid -->
-        </nav>
-
-        <!-- Main component for a primary marketing message or call to action -->
-        <div class="container">
-            <h4>Urutkan huruf berikut menjadi kata yang benar</h4>
-            <form class="form-horizontal" onsubmit="return submit_jawaban()">
-                <input type="hidden" name="word_id" id="word_id">
-                <div class="form-group"><label class="col-sm-2 control-label text-left">Score <span id="nilai">0</span></label></div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label text-left" id="pertanyaan"></label>
-                    <div class="col-sm-4">
-                        <input autofocus autocomplete="off" type="text" name="jawaban" id="jawaban" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-4">
-                        <button class="btn btn-default">Submit &raquo;</button>
-                        <button class="btn btn-default" onclick="location.href='<?php echo base_url() ?>'" >Reset</button>
-                    </div>
-                </div>
-            </form>
-        </div> */ ?>
     </div>
 </body>
     
